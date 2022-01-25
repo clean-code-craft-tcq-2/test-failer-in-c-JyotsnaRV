2422a86a147d91faebd5721df03eb826cc26c6f4
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-01-26 01:07:41 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
+void testNumberColorPairCode(int colorNum, int majorColorCode, int minorColorCode);
 
+int colorCodeCombo;
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
@@ -8,9 +10,30 @@ int printColorMap() {
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
             printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            testNumberColorPairCode(i*5+j,i,j);
         }
     }
     return i * j;
+}
+
+void testNumberColorPairCode(int colorNum, int majorColorCode, int minorColorCode)
+{
+    int expectedColorCombo;
+    if((colorNum+1) <= 5)
+    {
+        colorCodeCombo = -1;
+    }
+    else((colorNum+1)%5 == 0)
+    {
+        colorCodeCombo += 5; 
+    }
+    else
+    {
+        /* do nothing */
+    }
+    expectedColorCombo = (colorNum+1) + colorCodeCombo;
+    printf("actual color code \n pair number = %d \tcolor combo = %d\n",colorNum+1, expectedColorCombo);
+    assert((colorNum+1) == expectedColorCombo);
 }
 
 int main() {
